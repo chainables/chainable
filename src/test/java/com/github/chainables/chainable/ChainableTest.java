43ac7cf75b41ddf712a40b655127f2cd41db9cc1
Martin Sawicki <msawicki@alumni.princeton.edu>
2026-01-21 22:46:01 -0800

    Adding a Chainable#transform() overload for a function that accepts the ordinal number of an item in a chain

--- src/test/java/com/github/chainables/chainable/ChainableTest.java
+++ src/test/java/com/github/chainables/chainable/ChainableTest.java
@@ -1363,6 +1363,20 @@ public class ChainableTest {
     }
 
     @Test
+    public void testTransformWithIndex() {
+        // Given
+        Iterable<Integer> items = Arrays.asList(0, 1, 2, 3);
+
+        // When
+        Iterable<Integer> indices = Chainables.transform(items, (o, i) -> i);
+        int expected = 0;
+        for (int actual : indices) {
+            // Then
+            assertEquals(expected++, actual);
+        }
+    }
+
+    @Test
     public void testTransformAndFlatten() {
         // Given
         String[][] items = { { "a", "b" }, { "c", "d", "e" }, null, { "f" }};
